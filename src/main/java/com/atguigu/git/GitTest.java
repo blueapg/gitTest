24980955d934eb8d28057b945e6e83be46db2f0d
Linda <Linda@123.com>
2023-02-21 19:56:16 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 黄龙媛
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(" hello, git !");
+    }
+}
